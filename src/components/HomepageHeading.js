b893895623d3be52db292df37480017b5b87bc10
tyort <neotyo1986@gmail.com>
2023-02-11 12:00:57 +0300

    Доюавялем новый шрифт

--- src/components/HomepageHeading.js
+++ src/components/HomepageHeading.js
@@ -4,8 +4,18 @@ import { Button, Container, Header, Icon } from 'semantic-ui-react';
 function HomepageHeading() {
   return (
     <Container text>
-      <Header as='h1' content='Мцхета' inverted />
-      <Header as='h2' content='Грузинские блюда в печи на дровах' inverted />
+      <Header
+        as='h1'
+        content='Мцхета'
+        inverted
+        style={{ fontFamily: 'Arbor, Arial, sans-serif !important' }}
+      />
+      <Header
+        as='h2'
+        content='Грузинские блюда в печи на дровах'
+        inverted
+        style={{ fontFamily: 'Arbor, Arial, sans-serif !important' }}
+      />
       <Button primary size='huge'>
         Get Started
         <Icon name='right arrow' />
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import 'semantic-ui-css/semantic.min.css';
+import '../styles/fonts.scss';
 import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
