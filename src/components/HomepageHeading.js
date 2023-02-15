01e72969b81f58d0a2dc7759813774d18df5dc58
tyort <neotyo1986@gmail.com>
2023-02-16 01:01:03 +0300

    Меняем стилизацию заголовков в шапке сайта

--- src/components/HomepageHeading.js
+++ src/components/HomepageHeading.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import Image from 'next/image';
 import { Button, Container, Header, Icon } from 'semantic-ui-react';
 
 function HomepageHeading() {
   return (
     <Container text>
+      <Image
+        style={{ marginTop: '10vh' }}
+        src='/images/mountains.png'
+        width={150}
+        height={80}
+        alt='горы'
+      />
       <Header
         as='h1'
         content='Мцхета'
         inverted
-        style={{ fontFamily: 'Arbor, Arial, sans-serif !important' }}
+        style={{
+          fontFamily: 'Arbor, Arial, sans-serif !important',
+          marginTop: '0 !important',
+        }}
       />
       <Header
         as='h2'
         content='Грузинские блюда в печи на дровах'
         inverted
-        style={{ fontFamily: 'Arbor, Arial, sans-serif !important' }}
+        style={{
+          fontFamily: 'Arbor, Arial, sans-serif !important',
+          marginBottom: '2rem',
+        }}
       />
-      <Button primary size='huge'>
-        Get Started
-        <Icon name='right arrow' />
-      </Button>
     </Container>
   );
 }
