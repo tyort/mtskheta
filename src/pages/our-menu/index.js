b18d8d44e4f8fae4815653a6a019c582ef51f7a4
tyort <neotyo1986@gmail.com>
2023-03-01 12:24:01 +0300

    getStaticProps

--- src/pages/our-menu/index.js
+++ src/pages/our-menu/index.js
@@ -155,7 +155,7 @@ function OurMenu({ data }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   // Fetch data from external API
   const res = await fetch(`${process.env.BASE_FETCH_URL}/api/hello`);
   const data = await res.json();
