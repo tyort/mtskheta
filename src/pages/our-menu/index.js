cfd2a52367f9b26d796f240d8a8f304b304f2da7
tyort <neotyo1986@gmail.com>
2023-03-01 17:49:14 +0300

    Создаем страницу меню

--- src/pages/our-menu/index.js
+++ src/pages/our-menu/index.js
@@ -3,159 +3,87 @@ import { Comment, Header, Icon, Segment } from 'semantic-ui-react';
 import styles from '../../styles/Home.module.scss';
 
 function OurMenu({ data }) {
-  console.log(data);
   return (
     <>
-      <Segment style={{ backgroundColor: 'transparent' }} padded='very' raised>
-        <Image
-          src='/images/222.jpg'
-          alt='грузинская печь'
-          fill
-          style={{
-            objectFit: 'fill',
-            zIndex: -1,
-          }}
-        />
-        <Header
-          textAlign='center'
-          as='h2'
-          className='header-style__mon-amour-one'
-        >
-          Какой-то хедер
-        </Header>
-        <Comment.Group
-          style={{
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            maxWidth: '1200px',
-          }}
-        >
-          <Comment>
-            <Comment.Content>
-              <Comment.Author
-                className='header-style__mon-amour-one'
-                style={{
-                  margin: '0',
-                  display: 'table',
-                  borderCollapse: 'collapse',
-                  width: '100%',
-                }}
-              >
-                <p className={styles.product}>Хачапури по аджарски</p>
-                <p
-                  style={{
-                    display: 'table-cell',
-                    paddingBottom: '0.5rem',
-                    verticalAlign: 'bottom',
-                  }}
-                >
-                  asdasd
-                </p>
-              </Comment.Author>
-              <Comment.Metadata>
-                <div>2 days ago</div>
-                <div>
-                  <Icon name='star' />5 Faves
-                </div>
-              </Comment.Metadata>
-              <Comment.Text>Как-то атк бывает</Comment.Text>
-            </Comment.Content>
-          </Comment>
-          <Comment>
-            <Comment.Avatar as='a' src='/images/title-fireplace.jpg' />
-            <Comment.Content>
-              <Comment.Author>Stevie Feliciano</Comment.Author>
-              <Comment.Metadata>
-                <div>2 days ago</div>
-                <div>
-                  <Icon name='star' />5 Faves
-                </div>
-              </Comment.Metadata>
-              <Comment.Text
-                style={{
-                  margin: '1em 0',
-                  display: 'table',
-                  borderCollapse: 'collapse',
-                  width: '100%',
-                }}
-              >
-                <p className={styles.product}>
-                  Hey guys, I hope this example comment is helping you read this
-                  documentation
-                </p>
-                <p>asdasd</p>
-              </Comment.Text>
-            </Comment.Content>
-          </Comment>
-        </Comment.Group>
-      </Segment>
-      <Segment padded='very' piled raised secondary>
-        <Comment.Group style={{ maxWidth: 'none' }}>
-          <Comment>
-            <Comment.Content>
-              <Comment.Author
-                style={{
-                  margin: '0',
-                  display: 'table',
-                  borderCollapse: 'collapse',
-                  width: '100%',
-                }}
-              >
-                <p className={styles.product}>Хачапури по аджарски</p>
-                <p
-                  style={{
-                    display: 'table-cell',
-                    paddingBottom: '0.5rem',
-                    verticalAlign: 'bottom',
-                  }}
-                >
-                  asdasd
-                </p>
-              </Comment.Author>
-              <Comment.Metadata>
-                <div>2 days ago</div>
-                <div>
-                  <Icon name='star' />5 Faves
-                </div>
-              </Comment.Metadata>
-              <Comment.Text>Как-то атк бывает</Comment.Text>
-            </Comment.Content>
-          </Comment>
-          <Comment>
-            <Comment.Avatar as='a' src='/images/title-fireplace.jpg' />
-            <Comment.Content>
-              <Comment.Author>Stevie Feliciano</Comment.Author>
-              <Comment.Metadata>
-                <div>2 days ago</div>
-                <div>
-                  <Icon name='star' />5 Faves
-                </div>
-              </Comment.Metadata>
-              <Comment.Text
-                style={{
-                  margin: '1em 0',
-                  display: 'table',
-                  borderCollapse: 'collapse',
-                  width: '100%',
-                }}
-              >
-                <p className={styles.product}>
-                  Hey guys, I hope this example comment is helping you read this
-                  documentation
-                </p>
-                <p>asdasd</p>
-              </Comment.Text>
-            </Comment.Content>
-          </Comment>
-        </Comment.Group>
-      </Segment>
+      {data.map((sheet) => {
+        const { type, schedule } = sheet;
+        return (
+          <Segment
+            key={type}
+            style={{ backgroundColor: 'transparent' }}
+            padded='very'
+            raised
+          >
+            <Image
+              src='/images/222.jpg'
+              alt='грузинская печь'
+              fill
+              style={{
+                objectFit: 'fill',
+                zIndex: -1,
+              }}
+            />
+            <Header
+              textAlign='center'
+              as='h2'
+              className='header-style__mon-amour-one'
+            >
+              {type}
+            </Header>
+            <Comment.Group
+              style={{
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                maxWidth: '1200px',
+              }}
+            >
+              {schedule.map((dish) => {
+                const { title, price, description, portion } = dish;
+                return (
+                  <Comment key={`${title}${price}`}>
+                    <Comment.Content>
+                      <Comment.Author
+                        className='header-style__mon-amour-one'
+                        style={{
+                          margin: '0',
+                          display: 'table',
+                          borderCollapse: 'collapse',
+                          width: '100%',
+                        }}
+                      >
+                        <p className={styles.product}>{title}</p>
+                        <p
+                          style={{
+                            display: 'table-cell',
+                            paddingBottom: '0.5rem',
+                            verticalAlign: 'bottom',
+                          }}
+                        >
+                          {price}
+                        </p>
+                      </Comment.Author>
+                      <Comment.Metadata>
+                        <div>{portion}</div>
+                        <div>
+                          <Icon name='star' />5 Faves
+                        </div>
+                      </Comment.Metadata>
+                      <Comment.Text>{description}</Comment.Text>
+                    </Comment.Content>
+                  </Comment>
+                );
+              })}
+            </Comment.Group>
+          </Segment>
+        );
+      })}
     </>
   );
 }
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(`${process.env.BASE_FETCH_URL}/api/hello`);
+  const res = await fetch(`${process.env.BASE_FETCH_URL}/api/menu`);
   const data = await res.json();
 
   // Pass data to the page via props
